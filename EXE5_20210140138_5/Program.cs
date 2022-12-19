153abb664713acd0d86740fc93beada3d9c2ef30
calvin hendrawan <calvin.h.ft21@mail.umy.ac.id>
2022-12-19 15:43:37 +0700

    Add project files.

---
+++ EXE5_20210140138_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE5_20210140138_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
